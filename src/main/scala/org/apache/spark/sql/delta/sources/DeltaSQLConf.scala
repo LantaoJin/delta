f5450333441dbbaa9d33c496c31eac7436438ee2
Jose Torres <joseph.torres@databricks.com>
2020-08-27 03:29:19 +0800

    [ES-31086][DELTA] Don't allow NOT NULL constraints inside arrays and maps.
    ## What changes were proposed in this pull request?
    
    Don't allow NOT NULL constraints inside arrays and maps, since they can't be enforced there.
    
    Includes a fallback flag to allow the constraint (but properly unset the nullability flag) since this has been incorrectly allowed for a while.
    
    ## How was this patch tested?
    
    new unit tests
    
    Author: Jose Torres <joseph.torres@databricks.com>
    
    #11448 is resolved by jose-torres/notnullarr.
    
    GitOrigin-RevId: 3e84b5b8d7d5cd5e1755fa441d58aaee28ab1677
    
    (cherry picked from commit 7ffbaed75cb28569a58667e2f6f6d77f5605be90)

--- src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
+++ src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
@@ -344,6 +344,14 @@ object DeltaSQLConf {
       .checkValue(_ >= 0, "the version must be >= 0")
       .createOptional
 
+  val ALLOW_UNENFORCED_NOT_NULL_CONSTRAINTS =
+    buildConf("constraints.allowUnenforcedNotNull.enabled")
+      .internal()
+      .doc("If enabled, NOT NULL constraints within array and map types will be permitted in " +
+        "Delta table creation, even though Delta can't enforce them.")
+      .booleanConf
+      .createWithDefault(false)
+
   val REWRITE_LEFT_JOIN =
     buildConf("rewrite.leftJoin.enabled")
       .doc(
